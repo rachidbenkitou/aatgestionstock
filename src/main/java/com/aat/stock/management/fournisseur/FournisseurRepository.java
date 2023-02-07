1bb80d9a5847205f0539034ab1a60042f46c1f38
Rachid Benkitou <rachidbenkitou1@gmail.com>
2023-02-08 00:06:28 +0100

    nothing

--- src/main/java/com/aat/stock/management/fournisseur/FournisseurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aat.stock.management.fournisseur;
-
-import com.aat.stock.management.receptionnaire.Receptionnaire;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FournisseurRepository extends JpaRepository< Fournisseur,Long> {
-    Fournisseur findByIce(String ice);
-    void deleteByIce(String ice);
-}
--- src/main/java/com/aat/stock/management/matierePremiere/MatierePremiereRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aat.stock.management.matierePremiere;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MatierePremiereRepository extends JpaRepository<MatierePremiere, String> {
-    MatierePremiere findMatierePremiereByDesignation(String designation);
-}
